db5d777ab7320cfafbe30dfbeed6ad952c95da98
Remy Phan <phan.remy@gmail.com>
2020-01-16 14:42:48 +0100

    pages root done

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,27 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:home, :index]
+  skip_before_action :authenticate_user!, only: [:home, :index, :activites, :references, :evenements, :documents, :about_us, :contact]
 
   def home
   end
 
   def index
+  end
+
+  def activites
+  end
+
+  def references
+  end
+
+  def evenements
+  end
+
+  def documents
+  end
+
+  def about_us
+  end
+
+  def contact
   end
 end
